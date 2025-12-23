3fb952a5c4edb1d26518c5f76c8ca4a2e8aa4ea8
HarleyC0 <gerson_harley@hotmail.com>
2025-12-22 20:23:22 -0500

    add new project App habits gamification

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,5 +1,6 @@
 import ToDoImg from "@/assets/ToDo.png";
 import ContratosMlImg from "@/assets/ContratosML.png";
+import securityImg from "@/assets/security.py.png";
 
 
 function Projects() {
@@ -48,6 +49,27 @@ function Projects() {
                                         <div className="flex flex-wrap gap-2 mt-2">
                                             <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80">NodeJs</div>
                                             <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80">Express</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href="https://github.com/Crokwar/habit-gamification" target="_blank">
+                        <div className="relative rounded-lg overflow-hidden group border-gray-300 dark:border-border border-1">
+                            <div className="relative">
+                                <img src={securityImg} alt="" fetchPriority="high" sizes="(max-width: 768px) 100vw, 40vw" width="600" height="600" className="w-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"/>
+                                <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent flex items-end opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+                                    <div className="p-4">
+                                        <h1 className="text-white text-xl font-bold">AppWeb Habitos Gamificados</h1>
+                                        <p className="text-white"> EN DESARROLLO - Transforma tu rutina en un juego. <br /> 
+                                        Una plataforma de productividad que utiliza mecánicas de gamificación para convertir metas personales en logros alcanzables</p>
+                                        <div className="flex flex-wrap gap-2 mt-2">
+                                            <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80">Python</div>
+                                            <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80">FastAPI</div>
+                                            <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80">TypeScript</div>
+                                            <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80">React</div>                                        
                                         </div>
                                     </div>
                                 </div>
